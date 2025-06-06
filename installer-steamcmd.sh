ea38552f8564381379914305a0007ea005bced85
ilumos <ilumos@gmail.com>
2025-06-06 14:54:35 +0000

    Re-add exit on error. Catch error from SteamCMD. Add colours.

--- installer-steamcmd.sh
+++ installer-steamcmd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+
 # Set optional variables with defaults
 BASE_DIR="${BASE_DIR:-/mnt/server}"
 BASE_DIR="${BASE_DIR%/}" # Remove trailing slash
@@ -9,15 +12,21 @@ STEAMCMD_PASS="${STEAMCMD_PASS:-}"
 STEAMCMD_2FA="${STEAMCMD_2FA:-}"
 STEAMCMD_VALIDATE="${STEAMCMD_VALIDATE:-0}"
 
+# Set up color functions
+echo_info(){ echo -e "\033[0;32m$*\033[0m"; }
+echo_error(){ echo -e "\033[0;31m$*\033[0m" >&2; }
+echo_warning(){ echo -e "\033[0;33m$*\033[0m"; }
+
 # Require STEAMCMD_APPID to be set
 if [ -z "${STEAMCMD_APPID:-}" ]; then
-    echo "Error: STEAMCMD_APPID is required."
+    echo_error "Error: STEAMCMD_APPID is required"
     exit 1
 fi
 
 # Require STEAMCMD_VALIDATE to be 0 or 1
 if [[ "$STEAMCMD_VALIDATE" != [01] ]]; then
-    echo "STEAMCMD_VALIDATE must be 0 or 1."; exit 1
+    echo_error "STEAMCMD_VALIDATE must be 0 or 1"
+    exit 1
 fi
 
 # Prepare STEAMCMD_VALIDATE to use as SteamCMD argument
@@ -27,27 +36,31 @@ else
     STEAMCMD_VALIDATE=""
 fi
 
-echo "Downloading and installing SteamCMD"
+echo_info "Downloading and extracting SteamCMD"
 mkdir -p "${BASE_DIR}/steamcmd"
 curl -sSL "${STEAMCMD_URL}" | tar -xzvf - -C "${BASE_DIR}/steamcmd"
 
-echo "Preparing directories for SteamCMD"
+echo_info "Preparing directories for SteamCMD"
 mkdir -p "${BASE_DIR}/steamapps"
-cd "${BASE_DIR}/steamcmd" || { echo "Error: Failed to change directory."; exit 1; }
+cd "${BASE_DIR}/steamcmd"
 chown -R root:root "${BASE_DIR}"
 export HOME="${BASE_DIR}"
 
-echo "Downloading Steam app ${STEAMCMD_APPID}"
-"${BASE_DIR}/steamcmd/steamcmd.sh" +force_install_dir "${BASE_DIR}" \
-    +login "${STEAMCMD_USER}" "${STEAMCMD_PASS}" "${STEAMCMD_2FA}" \
-    +app_update "${STEAMCMD_APPID}" "${STEAMCMD_VALIDATE}" \
-    +quit
+echo_info "Starting SteamCMD and downloading Steam app ${STEAMCMD_APPID}"
+if {
+    "${BASE_DIR}/steamcmd/steamcmd.sh" +force_install_dir "${BASE_DIR}" \
+        +login "${STEAMCMD_USER}" "${STEAMCMD_PASS}" "${STEAMCMD_2FA}" \
+        +app_update "${STEAMCMD_APPID}" "${STEAMCMD_VALIDATE}" \
+        +quit
+}; then
+    echo_info "SteamCMD successfully downloaded, updated or validated Steam app ${STEAMCMD_APPID}"
+else
+    echo_warning "SteamCMD exited with code $?"
+fi
 
-echo "SteamCMD exit code: $?"
-
-echo "Copying Steam libraries into place"
+echo_info "Copying Steam libraries into place"
 mkdir -p "${BASE_DIR}/.steam/sdk32" "${BASE_DIR}/.steam/sdk64"
 cp -v "${BASE_DIR}/steamcmd/linux32/steamclient.so" "${BASE_DIR}/.steam/sdk32/steamclient.so"
 cp -v "${BASE_DIR}/steamcmd/linux64/steamclient.so" "${BASE_DIR}/.steam/sdk64/steamclient.so"
 
-echo "Successfully installed SteamCMD and Steam app {STEAMCMD_APPID}"
+echo_info "Successfully installed SteamCMD and Steam app ${STEAMCMD_APPID}"
