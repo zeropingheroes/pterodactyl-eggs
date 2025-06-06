3ae81e0b65339d1c660c3b886f7179f1a96c6178
ilumos <ilumos@gmail.com>
2025-06-06 13:53:30 +0000

    Move comments to echo output

--- installer-steamcmd.sh
+++ installer-steamcmd.sh
@@ -30,23 +30,25 @@ else
     STEAMCMD_VALIDATE=""
 fi
 
-# Download and install SteamCMD
+echo "Downloading and installing SteamCMD"
 mkdir -p "${BASE_DIR}/steamcmd"
 curl -sSL "${STEAMCMD_URL}" | tar -xzvf - -C "${BASE_DIR}/steamcmd"
 
-# Prepare directories for SteamCMD
+echo "Preparing directories for SteamCMD"
 mkdir -p "${BASE_DIR}/steamapps"
 cd "${BASE_DIR}/steamcmd" || { echo "Error: Failed to change directory."; exit 1; }
 chown -R root:root "${BASE_DIR}"
 export HOME="${BASE_DIR}"
 
-# Download game files
+echo "Downloading Steam app ${STEAMCMD_APPID}"
 "${BASE_DIR}/steamcmd/steamcmd.sh" +force_install_dir "${BASE_DIR}" \
     +login "${STEAMCMD_USER}" "${STEAMCMD_PASS}" "${STEAMCMD_2FA}" \
     +app_update "${STEAMCMD_APPID}" "${STEAMCMD_VALIDATE}" \
     +quit
 
-# Copy Steam libraries into place
+echo "Copying Steam libraries into place"
 mkdir -p "${BASE_DIR}/.steam/sdk32" "${BASE_DIR}/.steam/sdk64"
 cp -v "${BASE_DIR}/steamcmd/linux32/steamclient.so" "${BASE_DIR}/.steam/sdk32/steamclient.so"
 cp -v "${BASE_DIR}/steamcmd/linux64/steamclient.so" "${BASE_DIR}/.steam/sdk64/steamclient.so"
+
+echo "Done"
